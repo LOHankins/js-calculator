56488472e20badce1282e0d3fb69a6f38468b353
Louis <LOHankins@gmail.com>
2016-03-25 14:05:08 -0400

    Basic calculator functions

--- calculator.js
+++ calculator.js
@@ -1,7 +1,5 @@
 var calculator = {
-	num1: 0,
-	num2: 0,
-	exp: 1,
+
 	add: function(num1, num2) {
 		sum = (num1 + num2);
 		return sum;
@@ -19,18 +17,30 @@ var calculator = {
 		return quot;
 	},
 	pwr: function(num1, exp) {
-		if (exp === 0) {
+		if (exp === 1) {
 			return num1;
 		}
 		else {
-			ans = num1 * (pwr(num1, (exp = exp -1) ));
+			return num1 * (calculator.pwr(num1, (exp = exp -1) ));
+
 		}
 	},
 };
-var x = 10;
-var y = 20;
+//	var x = 5;
+//	var y = 2;
 
 
-var sm = 0;
-sm = calculator.add(x, y);
-console.log(sm);
+//	var sm = 0;
+//	sm = calculator.add(x, y);
+//	console.log(sm);
+//	sm = calculator.mult(x, y);
+//	console.log(sm);
+//	sm = calculator.sub(x, y);
+//	console.log(sm);
+//	sm = calculator.div(x, y);
+//	console.log(sm);
+//	sm = calculator.pwr(x, y);
+//	console.log(sm);
+
+console.log("Enter caculator.xxx(number1, number2) to try the program");
+console.log("Functions are add, mult, sub, div, and pwr for exponents");
